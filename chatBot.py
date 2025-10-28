cb609b7d3da403668d1016d4d70cbab9b164b141
GuilherGabriel07 <ggsantana18@gmail.com>
2025-10-27 22:24:16 -0300

    att7

--- chatBot.py
+++ chatBot.py
@@ -1,20 +1,29 @@
+
 import google.generativeai as genai
 from datetime import date
 import streamlit as st
 
-# Configuração da chave da API
 API_KEY = "AIzaSyAgHapNvvYUwTVP-GUuIr6Q7XIV6VCLivM"
 genai.configure(api_key=API_KEY)
 
-# Inicia o modelo do Gemini
 modelo = genai.GenerativeModel("gemini-2.5-flash")
+
 st.title("ChatBot com Gemini + Streamlit")
-pergunta = st.chat_input("Você: ")
-usuario = st.markdown(f'**Você**: {pergunta}')
-if  pergunta != 'cancelar':
-    
-    prompt = f"Hoje é dia {date.today()}. Pergunta do usuário: {pergunta}"
-    resposta = modelo.generate_content(prompt)
-    if resposta :
-     st.markdown(f"\r**IA:** {resposta.text}")
-    
+
+# Lógica para Gerenciar a Sessão de Chat
+if "chat_session" not in st.session_state:
+    st.session_state.chat_session = modelo.start_chat(history=[])
+
+# exibe historico rico da Conversa
+for message in st.session_state.chat_session.history:
+    st.markdown(f"*{'Você' if message.role == 'user' else 'IA'}*: {message.parts[0].text}")
+
+pergunta = st.chat_input("Insira sua pergunta ")
+prompt = f"Hoje é {date.today()}. Pergunta: {pergunta}"
+
+if pergunta:
+     
+ st.markdown(f"*Você*: {pergunta}")
+ # manda a resposta da IA para o historico da conversa
+ resposta = st.session_state.chat_session.send_message(prompt)
+ st.markdown(f"*IA*: {resposta.text}")
